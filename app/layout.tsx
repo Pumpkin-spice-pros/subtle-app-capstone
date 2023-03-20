bbf9c85026b0e73448807ecc5b045c7d96b5bc3b
Aaron-Celia <card123er@gmail.com>
2023-03-20 12:07:51 -0400

    css changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 // make layout background only for entire app
 
 import Head from "next/head";
+import styles from './page.module.css';
 
 export const metadata = {
     title: 'Subtle'
@@ -9,11 +10,15 @@ export default function Layout({ children }: { children: React.ReactNode}) {
   return (
 		<html>
 			<body
-				className=""
-				style={{
-					background: 'url("https://wallpaperaccess.com/full/358034.jpg")',
-                    boxSizing: 'border-box'
-				}}>
+				className={styles.bodyEle}
+				// style={{
+				// 	background:
+				// 		'url("https://images.pexels.com/photos/7894905/pexels-photo-7894905.jpeg?auto=compress&cs=tinysrgb&w=600")',
+				// 	boxSizing: "border-box",
+				// 	backgroundSize: "cover",
+                //     backgroundRepeat: 'no-repeat'
+				// }}>
+                >
 				{children}
 			</body>
 		</html>
