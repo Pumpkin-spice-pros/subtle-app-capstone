6c7b5b207c077b371dd72ad285349f0656447d20
AdamArabov <arabovadam@yahoo.com>
2023-03-21 14:39:46 -0400

    Front End Auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 // home landing page to show what the app is all about with buttons leading to login or sign up
+import AuthModal from "@/components/AuthModal";
 import "./globals.css";
 
 export default function Main() {
@@ -29,12 +30,9 @@ export default function Main() {
 					<h4>Login or Sign Up to get started</h4>
 				</div>
 				<div className="mt-10 ">
-					<button className="mr-5 inline-block rounded bg-black px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#14a44d] transition duration-150 ease-in-out hover:bg-success-600 hover:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] focus:bg-success-600 focus:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] focus:outline-none focus:ring-0 active:bg-success-700 active:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)]">
-						Login
-					</button>
-					<button className="inline-block rounded bg-green-800 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#14a44d] transition duration-150 ease-in-out hover:bg-success-600 hover:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] focus:bg-success-600 focus:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] focus:outline-none focus:ring-0 active:bg-success-700 active:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)]">
-						Sign Up
-					</button>
+					<AuthModal isLogin={true}/>
+					<br></br>
+					<AuthModal isLogin={false}/>
 				</div>
 			</div>
 		</div>
